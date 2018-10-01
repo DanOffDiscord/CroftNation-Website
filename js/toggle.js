f08a2b100b92fb647d903816a5e5be8b3bdcffa2
StupidDan <danielbee912@gmail.com>
2018-10-01 18:32:40 +0100

    Updated Nation Spotlight from InfernoGamingx to Gracie99 and added toggle button for showing Stream Embed

---
+++ js/toggle.js
@@ -0,0 +1,9 @@
+function toggle() {
+	var notification = document.getElementById('stream-embed');
+
+	if (notification.style.display == "block") {
+		notification.style.display = 'none';
+	} else {
+		notification.style.display = "block";
+	}
+}
